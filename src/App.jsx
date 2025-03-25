0b0f9cef75a278dc584006636f89db4671896586
Katoshi12 <bulat.bulatov.99@bk.ru>
2025-03-25 17:18:10 +0300

    Feature: semantic layout, divided by block components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./scss/app.scss"
+import Header from "./components/Header"
+import Categories from "./components/Categories"
+import Sort from "./components/Sort"
+import PizzaBlock from "./components/PizzaBlock.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="wrapper">
+      <Header/>
+      <main className="content">
+        <div className="container">
+
+          <section className="content__top">
+            <Categories/>
+            <Sort/>
+          </section>
+
+          <h2 className="content__title">Все пиццы</h2>
+
+          <section className="content__items">
+            <PizzaBlock title="Арабская" price="700"/>
+            <PizzaBlock title="Немецкая" price="600"/>
+          </section>
+        </div>
+      </main>
+    </div>
   )
 }
 
