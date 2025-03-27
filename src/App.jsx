7fb5388e43593ede4519cc47637c1b4a34323f9d
Katoshi12 <bulat.bulatov.99@bk.ru>
2025-03-27 10:30:23 +0300

    Feature: render dynamic card(pizza) and categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import "./scss/app.scss"
+import "./assets/scss/app.scss"
 import Header from "./components/Header"
 import Categories from "./components/Categories"
 import Sort from "./components/Sort"
 import PizzaBlock from "./components/PizzaBlock.jsx";
+import pizzaItem from "./assets/pizzas.json"
 
 function App() {
   return (
@@ -10,17 +11,20 @@ function App() {
       <Header/>
       <main className="content">
         <div className="container">
-
+          
           <section className="content__top">
             <Categories/>
             <Sort/>
           </section>
-
+          
           <h2 className="content__title">Все пиццы</h2>
-
+          
           <section className="content__items">
-            <PizzaBlock title="Арабская" price="700"/>
-            <PizzaBlock title="Немецкая" price="600"/>
+            {
+              pizzaItem.map((pizza) =>
+                <PizzaBlock { ...pizza }/>
+              )
+            }
           </section>
         </div>
       </main>
