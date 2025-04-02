167e450f5d815d66b470bc533ccda4169cf99f81
Katoshi12 <bulat.bulatov.99@bk.ru>
2025-04-02 18:24:49 +0300

    Feature: skeleton loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,47 @@
 import "./assets/scss/app.scss"
 
+import { useEffect, useState } from "react";
 import Header from "./components/Header"
 import Categories from "./components/Categories"
 import Sort from "./components/Sort"
-import PizzaBlock from "./components/PizzaBlock.jsx";
-import { useEffect, useState } from "react";
+import PizzaBlock from "./components/PizzaBlock";
+import Skeleton from "./components/PizzaBlock/Skeleton.jsx";
 
 
 function App() {
   const [pizzaItems, setPizzaItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
   const url = 'https://67e5487418194932a58561f5.mockapi.io/items'
-  
+
   useEffect(() => {
+
     fetch(url).then((response) => response.json())
       .then(json => {
         setPizzaItems(json)
+        setIsLoading(false)
       })
   }, [])
-  
+
   return (
     <div className="wrapper">
       <Header/>
       <main className="content">
         <div className="container">
-          
+
           <section className="content__top">
             <Categories/>
             <Sort/>
           </section>
-          
+
           <h2 className="content__title">Все пиццы</h2>
-          
+
           <section className="content__items">
             {
-              pizzaItems.map((pizza) =>
-                <PizzaBlock key={ pizza.id } { ...pizza }/>
-              )
+              isLoading ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+                  : pizzaItems.map(pizza => (
+                      <PizzaBlock key={pizza.id} pizza={pizza} />
+                  ))
             }
           </section>
         </div>
