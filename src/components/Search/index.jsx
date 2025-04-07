160562f22113b83cf8592bc0f5c88cfff455c56d
Katoshi12 <bulat.bulatov.99@bk.ru>
2025-04-07 14:46:03 +0300

    Perfomance improvement: optimize search input using lodash.debounce

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,9 +1,31 @@
-import styles from "./search.module.scss"
-import { useContext } from "react";
+import { useCallback, useContext, useRef, useState } from "react";
 import { SearchContext } from "../../App.jsx";
+import debounce from 'lodash.debounce'
+
+import styles from "./search.module.scss"
 
 const Search = () => {
-  const { searchQuery, setSearchQuery } = useContext(SearchContext)
+  const [value, setValue] = useState("");
+  const {setSearchQuery} = useContext(SearchContext)
+  const inputRef = useRef();
+
+  const onClear = () => {
+    setSearchQuery('');
+    setValue('');
+    inputRef.current.focus();
+  }
+
+  const getChangeInput = useCallback(
+    debounce((str) => {
+      setSearchQuery(str)
+    }, 250),
+    []
+  )
+
+  const setChangeInput = event => {
+    setValue(event.target.value)
+    getChangeInput(event.target.value)
+  }
 
   return (
     <div className={ styles.root }>
@@ -14,13 +36,14 @@ const Search = () => {
           d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"/>
       </svg>
       <input
-        value={ searchQuery }
-        onChange={ event => setSearchQuery(event.target.value) }
+        ref={ inputRef }
+        value={ value }
+        onChange={ setChangeInput }
         className={ styles.input }
         placeholder="Поиск пиццы..."
       />
-      { searchQuery && (
-        <svg onClick={ () => setSearchQuery('') } className={ styles.clearIcon } viewBox="0 0 20 20"
+      { value && (
+        <svg onClick={ onClear } className={ styles.clearIcon } viewBox="0 0 20 20"
              xmlns="http://www.w3.org/2000/svg">
           <path
             d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/>
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import filterSlice from "./slices/filterSlice.js";
+
 export const store = configureStore({
   reducer: {
     filterSlice,
